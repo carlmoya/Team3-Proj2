676d281d349460c4c504afaa020e3af52468a380
GreenIsaiah <ikragland@my.waketech.edu>
2025-10-05 16:53:35 -0400

    added roof added the  teleporting paintings, and made script
    added roofs (in level 2) made the painting (i made the painting that teleports up from ground to roof ( not rotateing but instead copy the roof  and flip it), and made script that deletes the wall that is blocking the path downstairs and shows the painting needed to get across the pit (when player enters the painting in the end of the corner)

---
+++ Assets/Scripts/Level2/CorrectPainting.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CorrectPainting : MonoBehaviour
+{
+    public GameObject destroyBlock;
+    public GameObject showPainting;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        showPainting.SetActive(false);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = new Vector3(746f, 19.85f, 302.52f);
+            showPainting.SetActive(true);
+            Destroy(destroyBlock);
+        }
+    }
+}
