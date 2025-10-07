307083476b22691726dd6e8baca6c77319485993
GreenIsaiah <ikragland@my.waketech.edu>
2025-10-07 15:17:03 -0400

    Lvl1 added lights, added materials/textures to walls
    added lights, added materials/textures to walls

---
+++ Assets/Scripts/Level1LightChange.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Level1LightChange : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (CompareTag("Player") && PlayerHasTreasure() == true)
+        {
+            //change lights to red
+        }
+        */
+    }
+}
