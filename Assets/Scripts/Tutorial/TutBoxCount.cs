da4819ea491412a514d64206f1f2793dadfbf59d
GreenIsaiah <ikragland@my.waketech.edu>
2025-10-13 17:47:06 -0400

    started making tutorial Level
    started making a tutorial Level

---
+++ Assets/Scripts/Tutorial/TutBoxCount.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class TutBoxCount : MonoBehaviour
+{
+    private int boxAmount = 0;
+
+    void Update()
+    {
+        if (boxAmount >=3)
+        {
+            //go to level 1
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Box"))
+        {
+            boxAmount += 1;
+            Destroy(other.gameObject);
+            Debug.Log( "You have " +  boxAmount + " boxes ");
+        }
+    }
+    /*
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Box"))
+        {
+            boxAmount -= 1;
+            Debug.Log("You have " + boxAmount + " boxes ");
+        }
+    }
+    */
+}
