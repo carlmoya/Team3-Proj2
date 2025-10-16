8b6f340cbfd162d900467d242addc632350a7a10
carlmoya <camoyaastudillo@my.waketech.edu>
2025-10-15 21:40:46 -0400

    Fixed level 3 progression bug.
    Stopped the progression method from being infinitely called by the door.

--- Assets/Scripts/LevelThreeDoor.cs
+++ Assets/Scripts/LevelThreeDoor.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 
 public class LevelThreeDoor : MonoBehaviour
 {
+    // Fields
+
+    private bool canProgress = true;
+
     // Trigger Methods
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player") && FindFirstObjectByType<BossController>() == null && FindFirstObjectByType<WeaponHealth>() == null)
+        if (other.CompareTag("Player") && FindFirstObjectByType<BossController>() == null && FindFirstObjectByType<WeaponHealth>() == null && canProgress == true)
         {
+            canProgress = false;
+
             FindFirstObjectByType<SceneTransitionController>().LoadMainMenu();
         }
     }
