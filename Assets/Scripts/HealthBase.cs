08d9199aa2eaeb160e671cc9f63eb4357576e65b
carlmoya <camoyaastudillo@my.waketech.edu>
2025-10-22 15:28:52 -0400

    Reverted level 3 lift behavior
    Also added invincibility frames to the health system

--- Assets/Scripts/HealthBase.cs
+++ Assets/Scripts/HealthBase.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public abstract class HealthBase : MonoBehaviour
@@ -6,16 +7,22 @@ public abstract class HealthBase : MonoBehaviour
 
     public int health = 1;
 
+    private bool isInvincible = false;
+
     // Methods
 
     public void Modify(int amount = 0) // Default parameter is 0
     {
+        if (isInvincible == true) { return; }
+
         // Modify health
         health += amount;
 
         if (amount < 0)
         {
             TakeDamage();
+
+            StartCoroutine(IFrames());
         }
 
         // Die if health runs out
@@ -28,4 +35,15 @@ public abstract class HealthBase : MonoBehaviour
     }
 
     public abstract void Die();
+
+    // Coroutines
+
+    private IEnumerator IFrames()
+    {
+        isInvincible = true;
+
+        yield return new WaitForSeconds(1.5f);
+
+        isInvincible = false;
+    }
 }
